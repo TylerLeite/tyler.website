6022dcb953b2b5f817922466717769515253093b
Tyler Leite <tyler@cooltyler.fun>
2020-12-02 10:43:34 -0800

    secret santa for seestor

---
+++ static/santa-seestor/js/santa.js
@@ -0,0 +1,30 @@
+function santa (seed) {
+  let peeps = ['Sophie', 'Allie', 'Issa', 'Hannah'];
+
+  function randomNumber (range) {
+    seed = (52667*seed + 20219) % 104729;
+    return seed % range;
+  }
+
+  function randomChoice (arr) {
+    return arr[randomNumber(arr.length)]
+  }
+
+  function shuffle (arr) {
+    for (let i = 0; i < randomNumber(200); i++) {
+      a = arr.splice(randomNumber(peeps.length), 1);
+      arr.push(a[0]);
+    }
+  }
+
+  shuffle(peeps);
+
+  let other = peeps.slice(1).concat([peeps[0]]);
+
+  let out = {};
+  for (let i = 0; i < peeps.length; i++) {
+    out[peeps[i]] = other[i]
+  }
+
+  return out;
+}
